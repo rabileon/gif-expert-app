7d1f2915fc730b119db731687d3b775d4a119f1d
Rabi León <rabi_leon@hotmail.com>
2022-02-12 15:52:06 -0600

    🚧 forms

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState([
+    'One Puch',
+    'Samurai X',
+    'Dragon Ball',
+  ]);
+
+  //   const handleAdd = () => {
+  //     // setCategories(['HunterXHunter', ...categories]);
+  //     setCategories((cats) => [...cats, 'HunterXHunter']);
+  //   };
   return (
     <>
       <h2>GifExpertApp</h2>
+      <AddCategory></AddCategory>
       <hr />
+      {/* <button onClick={handleAdd}> Agregar</button> */}
+      <ol>
+        {categories.map((category) => {
+          // El key sirve para react, ahorra que no se tenga que renderizar todo
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
   );
 };

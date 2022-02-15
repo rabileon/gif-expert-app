ed31eeb7e21ab0a89c14b1238fc6433fdb925978
Rabi León <rabi_leon@hotmail.com>
2022-02-14 21:52:35 -0600

    🚧  styles

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -3,7 +3,7 @@ import { AddCategory } from './components/AddCategory';
 import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
-  const [categories, setCategories] = useState(['One Puch']);
+  const [categories, setCategories] = useState(['pokemon']);
 
   //   const handleAdd = () => {
   //     // setCategories(['HunterXHunter', ...categories]);
